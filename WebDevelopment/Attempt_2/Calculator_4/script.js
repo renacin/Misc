1423df525df299d3064d41294540df9fa44e04e0
renacin <renacin.matadeen@ryerson.ca>
2020-05-10 17:57:30 -0400

    Making good progress, understand the general flow of javascript

--- WebDevelopment/Attempt_2/Calculator_4/script.js
+++ WebDevelopment/Attempt_2/Calculator_4/script.js
@@ -1,17 +1,57 @@
 
+// Create A Function So I can Iterate It Every Couple Of Seconds
+function UpdateClock() {
 
-
-function displayDate(){
+    // ---------------  Grab Date Information   ---------------
+    // Basic Date Information
     var myDate = new Date(),
-
-        date = myDate.getDate(),
-        month = myDate.getMonth(),
-        year = myDate.getFullYear();
+    day = myDate.getDate(),
+    month = myDate.getMonth(),
+    year = myDate.getFullYear();
 
     var months = ["January", "February", "March", "April", "May", "June",
                   "July", "August", "September", "October", "Novermber", "December"];
 
-    const date_str = months[month] + " " + date + " " + year;
+    var time_24hr = myDate.getHours();
+    var time_minutes = myDate.getMinutes();
+    var time_seconds = myDate.getSeconds();
 
-    document.getElementById("ShowDate").innerHTML = date_str;
+    // Determine Which Half Of The Day
+    var am_pm;
+
+    if (time_24hr >= 12) {am_pm = "PM";}
+    else if (time_24hr < 12) {am_pm = "AM";}
+
+    // ---------------  Grab Greeting Information   ---------------
+    // Determine The Appropriate Greeting For The Day
+    var greeting;
+
+    if (time_24hr >= 0 && time_24hr <= 6) {greeting = "Early Morning Regards Everyone!";}
+    else if (time_24hr >= 7 && time_24hr <= 11) {greeting = "Good Morning Everyone!";}
+    else if (time_24hr >= 12 && time_24hr <= 16) {greeting = "Good Afternoon Everyone!";}
+    else if (time_24hr >= 17 && time_24hr <= 20) {greeting = "Good Evening Everyone!";}
+    else if (time_24hr >= 23) {greeting = "Good Night Everyone!";}
+
+
+    // ---------------  Print Information To A Specific Tag   ---------------
+    var greeting_str = greeting
+    var date_str = months[month] + " " + day + " " + year
+    var time_str = time_24hr + ":" + time_minutes + ":" + time_seconds + " " + am_pm
+
+
+    document.getElementById("parsed_Greeting").innerHTML = greeting_str;
+    document.getElementById("parsed_Date").innerHTML = date_str;
+    document.getElementById("parsed_Time").innerHTML = time_str;
+
+
+    // ---------------  Print Information To Console   ---------------
+    // console.log(time_24hr, time_minutes, time_seconds, greeting)
+
 }
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// ---------------  Print Information To Console   ---------------
+setInterval(UpdateClock, 100); // Measured In Milliseconds
