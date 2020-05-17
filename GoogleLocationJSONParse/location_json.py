d464f34fcee52a93c78dff42853065ea0fce5dd1
renacin <renacin.matadeen@ryerson.ca>
2020-05-16 20:14:18 -0400

    Fix Activity Section

--- GoogleLocationJSONParse/location_json.py
+++ GoogleLocationJSONParse/location_json.py
@@ -4,29 +4,90 @@
 # Title                                          Overpass API Research
 #
 # ----------------------------------------------------------------------------------------------------------------------
-import requests
 import json
-# ----------------------------------------------------------------------------------------------------------------------
-"""
-Notes:
-
-"""
+import time
+import pandas as pd
+from datetime import datetime
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Def Function To Grab Data
-def grab_osm_data():
-    overpass_url = "http://overpass-api.de/api/interpreter"
+def grablocationdata(path):
 
-    overpass_query = """
-    node["amenity"="cafe"]({{bbox}}); out;
-    """
+    json_file = open(path)
+    json_file_str = json_file.read()
+    json_data = json.loads(json_file_str)
 
-    print("Sending Response")
-    response = requests.get(overpass_url, params={'data': overpass_query})
+    storage_dict = {"Date": [],
+                    "Time": [],
+                    "Latitude": [],
+                    "Longitude": [],
+                    "LatLongAcc": [],
+                    "Altitude": [],
+                    "Activity_Type": [],
+                    "Activity_Conf": []
+                    }
 
-    print("Gathering Data")
-    data = response.json()
+    for raw_data in json_data["locations"]:
+
+        # Capture Time Data
+        try:
+            time_data = int(raw_data["timestampMs"][0: 10])
+            time_raw = str(datetime.fromtimestamp(time_data))
+            time_split = time_raw.split(" ")
+
+            date_ = time_split[0]
+            time_ = time_split[1]
+        except KeyError as e:
+            date_ = ""
+            time_ = ""
+
+        # Capture Location
+        try:
+            latitude_ = raw_data["latitudeE7"] / 10**7
+            longitude_ = raw_data["longitudeE7"] / 10**7
+            acc_ = raw_data["accuracy"]
+        except KeyError as e:
+            latitude_ = ""
+            longitude_ = ""
+            acc_ = ""
+
+        # Additional Not Mission Critical | Capture Altitude Data
+        try:
+            altitude_ = raw_data["altitude"]
+        except KeyError as e:
+            altitude_ = ""
+
+        # Additional Not Mission Critical | Capture Activity Data
+        try:
+            act_r = raw_data["activity"]
+            act_type = act_r[0]["type"]
+            act_conf = act_r[0]["conf"]
+        except KeyError as e:
+            act_type = ""
+            act_conf = ""
+
+        # Append Data
+        storage_dict["Date"].append(date_)
+        storage_dict["Time"].append(time_)
+        storage_dict["Latitude"].append(latitude_)
+        storage_dict["Longitude"].append(longitude_)
+        storage_dict["LatLongAcc"].append(acc_)
+        storage_dict["Altitude"].append(altitude_)
+        storage_dict["Activity_Type"].append(act_type)
+        storage_dict["Activity_Conf"].append(act_conf)
+
+    # Dictionary To Pandas DF
+    print("Writing Data To DF")
+    df = pd.DataFrame.from_dict(storage_dict)
+
+    # Write To CSV
+    df.to_csv(r"C:\Users\renac\Desktop\LocationData.csv", index=False)
+    print("Finished Writting")
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    grab_osm_data()
+
+    path = r"C:\Users\renac\Desktop\LocationHistory.json"
+
+    grablocationdata(path)
