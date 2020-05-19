61ef84e015303a046aec39b6583cfdaa9c0aea69
renacin <renacin.matadeen@ryerson.ca>
2020-05-19 18:12:33 -0400

    Good Progress, Finish Up

--- HeritageStreetNameCleanUp/cleanup.py
+++ HeritageStreetNameCleanUp/cleanup.py
@@ -3,14 +3,62 @@
 # Title                                          HeritageStreetNameCleanUp
 #
 # ----------------------------------------------------------------------------------------------------------------------
-import requests
-import json
+import pandas as pd
+import re
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Def Function To Grab Data
-def cleanupstreetnames():
-    pass
 
+# SECONDARY FUNCTION | Determine How Many Addresses In The Street Row
+def addr_in_street_row(text):
+
+    #Returns String Of Digits, With Possibly An Alphabetic Character
+    pattern = r"\d+[A-Z]?"
+    list_of_addresses = re.findall(pattern, text)
+
+    # Return Just The Street Name
+    streetname = text
+    for addr in list_of_addresses:
+        streetname = streetname.replace(addr, "")
+
+    return list_of_addresses, streetname
+
+
+# MAIN FUNCTION | Clean Up Heritage Data
+def main():
+    # List Of Column Name
+    col_names = ["AssociatedProject", "Street","NumberOfAddresses", "DateOfConstruction",
+                "Typology", "Nominated_TF", "Nominated", "Decision", "Notes", "Status"]
+
+    # Create Dictionary To Store Cleaned Results
+    cleaned_data = {
+                    "AssociatedProject": [],
+                    "Street": [],
+                    "NumberOfAddresses": [],
+                    "DateOfConstruction": [],
+                    "Typology": [],
+                    "Nominated_TF": [],
+                    "Nominated": [],
+                    "Decision": [],
+                    "Notes": [],
+                    "Status": []
+                   }
+
+    # Import CSV
+    heritage_df = pd.read_csv(r"C:\Users\renac\Desktop\Heritage\HeritageSites_C.csv")
+
+    # Loop Through Each Row
+    for index, row in heritage_df.iterrows():
+
+        # How Many Streets?
+        list_of_addresses, streetname = addr_in_street_row(row["Street"])
+
+        # If 1 Address Just Append To Data, Else Iterate Through & Create Individual Addresses
+        if (len(list_of_addresses) == 1):
+            for col in col_names:
+                cleaned_data[col].append(row[col])
+
+        print(cleaned_data)
+        break
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    cleanupstreetnames()
+    main()
