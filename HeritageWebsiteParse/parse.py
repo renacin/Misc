8f2470c5ee956980f6b52b39b37f32980f66e386
renacin <renacin.matadeen@ryerson.ca>
2020-05-20 22:23:56 -0400

    Webscraper is ready

--- HeritageWebsiteParse/parse.py
+++ HeritageWebsiteParse/parse.py
@@ -6,6 +6,7 @@
 import re
 import time
 import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 # ----------------------------------------------------------------------------------------------------------------------
@@ -30,7 +31,39 @@ def parsedata(fullpath):
         # Visit Site
         chrome.get(fullpath)
         time.sleep(2)
+
+        # Grab Location Data | Match With Search Pattern
+        location_path = chrome.find_element_by_xpath("//*[@id=\"container\"]/div[7]/div[2]/span")
+        location_text = location_path.get_attribute('innerText')
+
+        try:
+            loc_info = location_text.split("\n")
+            name_ = loc_info[0]
+            addr_ = loc_info[1]
+            genloc_ = loc_info[2]
+
+        except:
+            name_ = fullpath
+            addr_ = ""
+            genloc_ = ""
+
+        # Grab Construction Data | Match With Search Pattern
+        construction_path = chrome.find_element_by_xpath("//*[@id=\"container\"]/div[11]")
+        construction_text = construction_path.get_attribute('innerText')
+
+        try:
+            year_info = construction_text.split("\n")
+            year_ = year_info[1]
+
+        except:
+            year_ = ""
+
+
+        # Close Driver & Append Data
         chrome.close()
+        print("School: {}, Address: {}, Neighbourhood: {}, Construction Date: {}".format(name_, addr_, genloc_, year_))
+        return name_, addr_, genloc_, year_
+
 
 # Main Function
 def main():
@@ -43,6 +76,9 @@ def main():
     # Grab IDs with RegEx
     listofids = grab_ids(text_raw)
 
+    # Dictionary That Will Store Everything
+    data_dictionary = {"School": [], "Address": [], "Neighbourhood": [], "Construction": []}
+
     # With List Of IDs Iterate Through Each And Scrape Data, Append To An Awaiting DataDictionry
     website = "https://www.acotoronto.ca/show_building.php?"
     for ids in listofids:
@@ -52,9 +88,19 @@ def main():
         full_path = website + cleaned_id
 
         # Parse Data
-        parsedata(full_path)
+        name_, addr_, genloc_, year_ = parsedata(full_path)
 
+        # Append To Dictionary
+        data_dictionary["School"].append(name_)
+        data_dictionary["Address"].append(addr_)
+        data_dictionary["Neighbourhood"].append(genloc_)
+        data_dictionary["Construction"].append(year_)
         break
+
+    # Write Data To CSV
+    final_df = pd.DataFrame.from_dict(data_dictionary)
+    final_df.to_csv(r"C:\Users\renac\Desktop\CityPatternsProject\SchoolConstructionDates.csv", index=False)
+    print("Finished Writting")
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
