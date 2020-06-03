0936c773ecdfe383e86d992de757fd2b3b8ab916
renacin <renacin.matadeen@ryerson.ca>
2020-06-03 12:35:46 -0400

    Should Loop Through DIR Expanded List Multiple Times - Don't Break Just After One

--- StreetName_CleanUp/Functions_Dir/cln_func.py
+++ StreetName_CleanUp/Functions_Dir/cln_func.py
@@ -24,86 +24,101 @@ def expand_dir(street_text):
     # For Each Entry In The Dictionary Try To Replace
     for key_, value_ in dir_dictionary.items():
 
-        str_repl = " " + value_[0] + " "
-        key_repl = " " + key_ + " "
+        # Try To Replace
+        str_repl = " " + str(value_[0]) + " "
+        key_repl = " " + str(key_) + " "
         dir_expand = street_text_edge.replace(str_repl, key_repl)
 
-    return dir_expand
+        # If Value Expanded break out Of Loop
+        if len(dir_expand) > len(street_text_edge):
+            break
+
+    return_val = str(dir_expand)
+    return return_val
 
 
+# SECONDARY FUNCTION | Expand Street Type For Easier Comparison & Search
+def expand_str(street_text):
 
-# # SECONDARY FUNCTION | Expand Street Type For Easier Comparisn & Search
-# def expand_str(street_text):
-#
-#     # Street Abbreviations Stored Here | BE CAREFUL OF CANADIAN SPELLING | FOR TORONTO ONLY | BETTER AS JSON?
-#     street_dictionary = {
-#                         "ALLEY" : ["ALLEE", "ALLEY", "ALLY", "ALY"],
-#                         "ANEX" : ["ANEX", "ANNEX", "ANNX", "ANX"],
-#                         "AVENUE" : ["AV", "AVE" , "AVEN" , "AVENU" , "AVENUE" , "AVN" , "AVNUE"],
-#                         "BOTTOM" : ["BOT", "BTM", "BOTTM"],
-#                         "BOULEVARD" : ["BLVD", "BOUL", "BOULV", "BLVRD"],
-#                         "CENTRE" : ["CEN", "CTR", "CENT", "CENTR", "CENTR", "CNTER", "CNTR"],
-#                         "COMMON" : ["CMN"],
-#                         "CORNER" : ["COR"],
-#                         "COURSE" : ["CRSE"],
-#                         "COURT" : ["CRT", "CT"],
-#                         "CREEK" : ["CRK"],
-#                         "CRESCENT" : ["CRES", "CRSENT", "CRSNT", "CRSNT"],
-#                         "CROSSING" : ["CROSSING", "XING", "CRSSNG"],
-#                         "CIRCLE" : ["CRCL"],
-#                         "DRIVE" : ["DR", "DRIV", "DRV"],
-#                         "EXPRESSWAY" : ["EXP", "EXPY", "EXPR", "EXPRESS", "EXPW", "EXPY"],
-#                         "GARDEN" : ["GDN", "GARDN", "GRDEN", "GRDN"],
-#                         "GARDENS" : ["GDNS"],
-#                         "GATE" : ["GT"],
-#                         "GROVE" : ["GRV"],
-#                         "GATEWAY" : ["GTWY", "GATEWY", "GATWAY", "GTWAY", "GTWAY", "GTWY"],
-#                         "HIGHWAY" : ["HWY", "HIGHWY", "HIWAY", "HIWY", "HWAY"],
-#                         "HILL" : ["HL"],
-#                         "HEIGHTS" : ["HTS"],
-#                         "JUNCTION" : ["JCT", "JCTION", "JUNCTN", "JUNCTON"],
-#                         "LANE" : ["LN", "Ln"],
-#                         "LAWN" : ["LWN"],
-#                         "PARK" : ["PRK", "PK"],
-#                         "PARKWAY" : ["PARKWY", "PKWAY", "PKWY", "PKY"],
-#                         "PATH" : ["PTH"],
-#                         "PLAZA" : ["PLZ", "PLZA"],
-#                         "PLACE" : ["PL"],
-#                         "POINT" : ["PT"],
-#                         "RIVER" : ["RIV", "RIVR", "RVR"],
-#                         "ROAD" : ["RD"],
-#                         "ROUTE" : ["RTE"],
-#                         "ROADWAY" : ["ROADWAY", "RDWY"],
-#                         "SHORE" : ["SHOAR", "SHR"],
-#                         "SQUARE" : ["SQ", "SQR", "SQRE", "SQU"],
-#                         "STATION" : ["STA", "STATN", "STN"],
-#                         "STREET" : ["ST", "STR", "STRT"],
-#                         "TRAIL" : ["TRL"],
-#                         "TERRACE" : ["TER"],
-#                         "VILLE" : ["VL", "VIL"],
-#                         "WAY" : ["WY"],
-#                         "WOODS" : ["WDS"]
-#                         }
-#
-#
-#     # For Each Entry In The Dictionary Compare And Exand The Street Type | Set Default Just Incase
-#     str_expand = street_text
-#     for key_, value_ in street_dictionary.items():
-#
-#         # Look For Match And Query Match Type
-#         common_val = list(set(street_as_list) & set(value_))
-#
-#         if len(common_val) > 0:
-#             str_repl = " " + common_val[0] + " "
-#             key_repl = " " + key_ + " "
-#             str_expand = street_text_edge.replace(str_repl, key_repl)
-#             break
+    # Street Abbreviations Stored Here | BE CAREFUL OF CANADIAN SPELLING | FOR TORONTO ONLY | BETTER AS JSON?
+    street_dictionary = {
+                        "ALLEY" : ["ALLEE", "ALLEY", "ALLY", "ALY"],
+                        "ANEX" : ["ANEX", "ANNEX", "ANNX", "ANX"],
+                        "AVENUE" : ["AV", "AVE" , "AVEN" , "AVENU" , "AVENUE" , "AVN" , "AVNUE"],
+                        "BOTTOM" : ["BOT", "BTM", "BOTTM"],
+                        "BOULEVARD" : ["BLVD", "BOUL", "BOULV", "BLVRD"],
+                        "CENTRE" : ["CEN", "CTR", "CENT", "CENTR", "CENTR", "CNTER", "CNTR"],
+                        "COMMON" : ["CMN"],
+                        "CORNER" : ["COR"],
+                        "COURSE" : ["CRSE"],
+                        "COURT" : ["CRT", "CT"],
+                        "CREEK" : ["CRK"],
+                        "CRESCENT" : ["CRES", "CRSENT", "CRSNT", "CRSNT"],
+                        "CROSSING" : ["CROSSING", "XING", "CRSSNG"],
+                        "CIRCLE" : ["CRCL"],
+                        "DRIVE" : ["DR", "DRIV", "DRV"],
+                        "EXPRESSWAY" : ["EXP", "EXPY", "EXPR", "EXPRESS", "EXPW", "EXPY"],
+                        "GARDEN" : ["GDN", "GARDN", "GRDEN", "GRDN"],
+                        "GARDENS" : ["GDNS"],
+                        "GATE" : ["GT"],
+                        "GROVE" : ["GRV"],
+                        "GATEWAY" : ["GTWY", "GATEWY", "GATWAY", "GTWAY", "GTWAY", "GTWY"],
+                        "HIGHWAY" : ["HWY", "HIGHWY", "HIWAY", "HIWY", "HWAY"],
+                        "HILL" : ["HL"],
+                        "HEIGHTS" : ["HTS"],
+                        "JUNCTION" : ["JCT", "JCTION", "JUNCTN", "JUNCTON"],
+                        "LANE" : ["LN", "Ln"],
+                        "LAWN" : ["LWN"],
+                        "PARK" : ["PRK", "PK"],
+                        "PARKWAY" : ["PARKWY", "PKWAY", "PKWY", "PKY"],
+                        "PATH" : ["PTH"],
+                        "PLAZA" : ["PLZ", "PLZA"],
+                        "PLACE" : ["PL"],
+                        "POINT" : ["PT"],
+                        "RIVER" : ["RIV", "RIVR", "RVR"],
+                        "ROAD" : ["RD"],
+                        "ROUTE" : ["RTE"],
+                        "ROADWAY" : ["ROADWAY", "RDWY"],
+                        "SHORE" : ["SHOAR", "SHR"],
+                        "SQUARE" : ["SQ", "SQR", "SQRE", "SQU"],
+                        "STATION" : ["STA", "STATN", "STN"],
+                        "STREET" : ["ST", "STR", "STRT"],
+                        "TRAIL" : ["TRL"],
+                        "TERRACE" : ["TER"],
+                        "VILLE" : ["VL", "VIL"],
+                        "WAY" : ["WY"],
+                        "WOODS" : ["WDS"]
+                        }
+
+    # Prelim Text Cleaning
+    street_text = street_text.upper()
+    street_as_list = street_text.split(" ")
+    street_text_edge = street_text + " "
+
+
+    # For Each Entry In The Dictionary Compare And Exand The Street Type | Set Default Just Incase
+    str_expand = street_text
+    for key_, value_ in street_dictionary.items():
+
+        # Look For Match And Query Match Type
+        common_val = list(set(street_as_list) & set(value_))
+
+        if len(common_val) > 0:
+            str_repl = " " + common_val[0] + " "
+            key_repl = " " + key_ + " "
+            str_expand = street_text_edge.replace(str_repl, key_repl)
+            break
+
+    return str_expand
 
 
 # MAIN FUNCTION | Clean Street Input
 def main_clean(street_text):
 
     dir_expanded = expand_dir(street_text)
+    str_cleaned = expand_str(dir_expanded)
+
+    return str_cleaned
 
 
 # ----------------------------------------------------------------------------------------------------------------------
--- StreetName_CleanUp/main.py
+++ StreetName_CleanUp/main.py
@@ -28,16 +28,14 @@ def main():
 
         # Clean Street Inputs
         cln_str = main_clean(row[street_name_col])
-        print(cln_str)
-        break
 
-    #     data_dictionary["Raw_Address"].append(row[street_name_col])
-    #     data_dictionary["Cleaned_Address"].append(cln_street)
-    #
-    # # DEBUGGING
-    # final_df = pd.DataFrame.from_dict(data_dictionary)
-    # final_df.to_csv(r"C:\Users\renac\Desktop\Test.csv", index=False)
-    # print("Finished Writting")
+        data_dictionary["Raw_Address"].append(row[street_name_col])
+        data_dictionary["Cleaned_Address"].append(cln_str)
+
+    # DEBUGGING
+    final_df = pd.DataFrame.from_dict(data_dictionary)
+    final_df.to_csv(r"C:\Users\renac\Desktop\Test.csv", index=False)
+    print("Finished Writting")
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
