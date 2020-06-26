a735904fa395f60eaa7e8ec6a37decc4941253e1
renacin <renacin.matadeen@ryerson.ca>
2020-06-26 19:43:02 -0400

    Need More Research

--- BigOExamples/BigO_Notes.py
+++ BigOExamples/BigO_Notes.py
@@ -7,6 +7,16 @@
 General:
     - Examples From:
         https://stackabuse.com/big-o-notation-and-algorithm-analysis-with-python-examples/
+        https://towardsdatascience.com/understanding-time-complexity-with-python-examples-2bda6e8158a7
+
+    - Big O In Order Of Time Complexity:
+        = O(1) ----------- Constant
+        = O(logN) -------- Logarithmic
+        = O(logN^C) ------ PolyLogarithmic
+        = O(N) ----------- Linear
+        = O(N^C) --------- Polynomial
+        = O(C^N) --------- Exponential
+        = O(N!) ---------- Factorial
 """
 # ----------------------------------------------------------------------------------------------------------------------
 """
@@ -23,6 +33,7 @@ Explanation:
 """
 # ----------------------------------------------------------------------------------------------------------------------
 """
+
 [EXAMPLE #2A - Linear Complexity]
 Ex:
     for name in listofnames:
@@ -46,11 +57,46 @@ Ex:
     for first_name in list_of_first_names:
         print(first_name)
 
-
-
 BigO:
     = O(1) + O(N) + O(1) + O(N)
     = O(2) + O(2N)
+    = O(2N)
 
 
+[EXAMPLE #3A - Polynomial Complexity]
+Ex:
+    for first_name in list_of_first_names:
+        for last_name in list_of_last_names:
+            print(first_name, last_name)
+
+BigO:
+    = O(N^2)
+
+Explanation:
+    - For each item in the outer loop, loop through the values of the inner loop
+    - Simply stated, N*N (For the number of nested loops)
+
+
+[EXAMPLE #3B - Complex Polynomial Complexity]
+Ex:
+    for first_name in list_of_first_names:
+        for last_name in list_of_last_names:
+            for nation in nation_list
+                print(first_name, last_name, nation)
+
+    print("Hello World")
+
+    for first_name in list_of_first_names:
+        print(first_name)
+
+BigO:
+    = O(N^3) + O(1) O(N)
+    = O(N^3)
+
+
+[EXAMPLE #4 - Exponential Complexity]
+Ex:
+    for first_name in list_of_first_names:
+        for last_name in list_of_last_names:
+            print(first_name, last_name)
 """
