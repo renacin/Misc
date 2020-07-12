4b38ddf00ef487ed34df01e8c5304ded0c158a0a
renacin <renacin.matadeen@ryerson.ca>
2020-07-12 16:24:32 -0400

    Added Back Tail Check Func.

--- Learning/ConceptsOfComputerScience/DataStructures/LinkedLists/singlylinkedlists.py
+++ Learning/ConceptsOfComputerScience/DataStructures/LinkedLists/singlylinkedlists.py
@@ -22,6 +22,7 @@ class SinglyLinkedList():
     def __init__(self):
         """ The Linked List Will Instatiate And Set Its Head None - No Nodes """
         self.head = None
+        self.tail = None
         self._lenght = 0
 
 
@@ -31,8 +32,15 @@ class SinglyLinkedList():
 
     def insert_top(self, input_data):
         """ Insert A New Node Of Data At The Top Of The List | Takes O(1)"""
-        if (self.head is None):
+        if (self.head is None):                                               # If List Is Empty
             new_node = self.Node(input_data)
+            self.head = new_node
+            self._lenght += 1
+            return
+
+        elif (self.head is not None) and (self.tail is None):                 # If Head Is Full, But Tail Empty
+            new_node = self.Node(input_data, self.head)
+            self.tail = self.head
             self.head = new_node
             self._lenght += 1
             return
@@ -49,15 +57,22 @@ class SinglyLinkedList():
         to in the opposite direction. Once finished, the head of your original list is now the head of your reversed
         list.
         """
+        counter = 0
         prev = None                             # Set previous node to node
         itr = self.head                         # Set your iterable, start at the top of your linked list
+
         while itr:                              # While self.head does not equal to None
+
+            if (counter == 0):
+                self.tail = itr
+                counter += 1
+
             temp = itr                          # Temp variable is equal to the node that your at
             itr = itr.next                      # itr is now the next node
             temp.next = prev                    # Your temp node (current node) now points to the previous
             prev = temp                         # Your previous now equals the current node
-        self.head = prev                        # Set the new head of you linked list as prev. Contains new pointers
 
+        self.head = prev                        # Set the new head of you linked list as prev. Contains new pointers
 
 
     def draw(self):
@@ -73,7 +88,19 @@ class SinglyLinkedList():
 
         return llstr[:-6]
 
+    def top_element(self):
+        """ Return The Element Currently On Top Of The LL | Takes O(1)"""
+        if (self.head is None):
+            return
 
+        return self.head.data
+
+    def last_element(self):
+        """ Return The Element Currently On Top Of The LL | Takes O(1)"""
+        if (self.tail is None):
+            return
+
+        return self.tail.data
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -89,9 +116,11 @@ def main():
         ll.insert_top(country)
 
     print("Linked List Lenght: {}".format(ll.get_lenght()))
-    print("Linked List: {}".format(ll.draw()))
+
     ll.reverse()
-    print("Linked List: {}".format(ll.draw()))
+    print("Top Element: {}".format(ll.top_element()))
+    print("Entire Linked List: {}".format(ll.draw()))
+    print("Last Element: {}".format(ll.last_element()))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
