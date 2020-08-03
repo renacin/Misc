f36baccdaeee10dec434c5bb50e35801a9a0be58
renacin <renacin.matadeen@ryerson.ca>
2020-08-03 18:25:27 -0400

    After a certain point, only then does GPU compute become viable. In this case it was 500 Million points

--- SideProjects/GPU_Programming/using_gpu.py
+++ SideProjects/GPU_Programming/using_gpu.py
@@ -4,25 +4,55 @@
 #
 # ----------------------------------------------------------------------------------------------------------------------
 import time
+import random
 import numpy as np
-from numba import vectorize
+from numba import jit, njit, vectorize, cuda
 # ----------------------------------------------------------------------------------------------------------------------
+@jit
+def original_computation(input_list):
+    output_list = []
+    for val in input_list:
+        if (val % 2 == 0):
+            output_list.append(2)
+        else:
+            output_list.append(1)
 
-@vectorize(['float32(float32, float32)'], target='cuda')
-def pow(a, b):
-    return a ** b
+    return output_list
+
+@vectorize
+def scalar_computation(num):
+    if (num % 2 == 0):
+        return 2
+    return 1
+
+    return output_list
+
+@vectorize(['int32(int32)'], target='cuda')
+def gpu_scalar_computation(num):
+    if (num % 2 == 0):
+        return 2
+    return 1
+
+    return output_list
 
 def main():
-    vec_size = 500_000_000
+    # List Size
+    input_list = np.arange(500_000_000)
 
-    a = b = np.array(np.random.sample(vec_size), dtype=np.float32)
-    c = np.zeros(vec_size, dtype=np.float32)
-
+    # Jitted Function
     start = time.time()
-    c = pow(a, b)
-    duration = time.time() - start
+    output_ = original_computation(input_list)
+    print("Jitted Function: {}".format(time.time() - start))
 
-    print(duration)
+    # Vectorized Function
+    start = time.time()
+    output_ = scalar_computation(input_list)
+    print("Vectorized Function On CPU: {}".format(time.time() - start))
+
+    # GPU Vectorized Function
+    start = time.time()
+    output_ = gpu_scalar_computation(input_list)
+    print("Vectorized Function On GPU: {}".format(time.time() - start))
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
