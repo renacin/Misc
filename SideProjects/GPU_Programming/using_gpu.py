7e249253dd1a2cd5707a43ee44237a6c6e5c2037
renacin <renacin.matadeen@ryerson.ca>
2020-08-03 16:52:14 -0400

    GPU Compute

---
+++ SideProjects/GPU_Programming/using_gpu.py
@@ -0,0 +1,28 @@
+# Name:                                            Renacin Matadeen
+# Date:                                               08/03/2020
+# Title                               Parallel Processing - Speeding Up Code With GPU
+#
+# ----------------------------------------------------------------------------------------------------------------------
+import numpy as np
+from timeit import default_timer as timer
+from numba import vectorize
+# ----------------------------------------------------------------------------------------------------------------------
+
+@vectorize(['float32(float32, float32)'], target='cuda')
+def pow(a, b):
+    return a ** b
+
+def main():
+    vec_size = 100000000
+
+    a = b = np.array(np.random.sample(vec_size), dtype=np.float32)
+    c = np.zeros(vec_size, dtype=np.float32)
+
+    start = timer()
+    c = pow(a, b)
+    duration = timer() - start
+
+    print(duration)
+
+if __name__ == '__main__':
+    main()
