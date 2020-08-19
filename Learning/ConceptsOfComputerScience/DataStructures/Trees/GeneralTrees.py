14536a5fed369942f714889b011a9270a8d91a15
renacin <renacin.matadeen@ryerson.ca>
2020-08-19 12:12:06 -0400

    Slow Progress

--- Learning/ConceptsOfComputerScience/DataStructures/Trees/GeneralTrees.py
+++ Learning/ConceptsOfComputerScience/DataStructures/Trees/GeneralTrees.py
@@ -8,29 +8,20 @@ import time
 
 
 class Node:
-    """ This will be a tree node """
+    """ This will be a General Tree node """
 
     def __init__(self, data):
         """ Basics For Each Node"""
-        self.data = data
+        self.parent = None
         self.children = []
+        self.data = data
 
-    def insert(self, data):
-        """ This method will be attached to a node """
-        # Compare the new value with the parent node
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-            self.data = data
+
+    def add_child(self, child):
+        """ This method will add a Node, as a child to your current Node """
+        child.parent = self
+        self.children.append(child)
+
 
 
 # ----------------------------------------------------------------------------------------------------------------------
