8c7ed6334d136124a04db1088131e6c5bc60b3d6
renacin <renacin.matadeen@ryerson.ca>
2020-11-30 22:33:19 -0500

    Reorganized & Better Layout Code; Very Messy

--- SideProjects/TorontoPoliceAuctions_SQLite/main.py
+++ SideProjects/TorontoPoliceAuctions_SQLite/main.py
@@ -4,6 +4,7 @@
 #
 # ----------------------------------------------------------------------------------------------------------------------
 import time
+import re
 from selenium import webdriver
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -17,6 +18,31 @@ def choose_url(choice):
     return base_url + cat_dict[choice] + "?page=0"
 
 
+
+# Parse Listing ID
+def listing_ids(raw_html):
+
+    # Find All Instances Of Listings With Regular Expression Pattern
+    listing_pattern = r'/Listing/Details/(.*)" class="btn btn-primary'
+    instances = re.findall(listing_pattern, raw_html)
+
+    return instances
+
+# Parse Total Number Of Pages
+def num_pages(chrome_driver):
+    # How Many More Pages?
+    pages_html = chrome_driver.find_element_by_xpath('/html/body/main/div/div[2]/div[3]/ul')
+    pages_text = pages_html.text
+    for arrow in ["«", "»", " "]:
+        pages_text = pages_text.replace(arrow, "")
+
+    all_pages = pages_text.split("\n")
+    return all_pages[-2]
+
+
+
+
+
 # A Function That Will Use A ChromeDriver To Navigate To The Site Chosen
 def visit_site(url):
     # Setup Chrome Driver
@@ -26,6 +52,15 @@ def visit_site(url):
     # Visit Site | Get Source HTML
     chrome.get(url)
     time.sleep(2)
+
+    # Grab Raw HTML
+    html_ = chrome.page_source
+
+    # Return Listing IDs As A List
+    listings = listing_ids(html_)
+
+    # Num Pages
+    print(num_pages(chrome))
 
     # Close Web Driver
     chrome.quit()
