ce3c4d2d7857d2c9f73a479d7c9163ee6e6a1c79
renacin <renacin.matadeen@ryerson.ca>
2020-12-11 18:43:27 -0500

    Understand Basics Of SQLite3 Must Implement Into Main Project Code

--- SideProjects/Basic_SQLite/main.py
+++ SideProjects/Basic_SQLite/main.py
@@ -18,8 +18,8 @@ class SQL_Database:
             # Connect To Database Check If It has Data In It
             self.conn = sqlite3.connect(r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\Basic_SQLite\Test.db")
             self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS Test
-            (NAME TEXT NOT NULL, AGE INT NOT NULL)
+            CREATE TABLE IF NOT EXISTS IdentifierTable
+            (ID INT NOT NULL, AGE INT NOT NULL)
             ;''')
 
             print("Connected To Database")
@@ -27,22 +27,35 @@ class SQL_Database:
         except sqlite3.OperationalError as e:
             print(e)
 
+
+    # Function To Insert Data Into Database
+    def addtoDB(self, data_):
+        cursor = self.conn.cursor()
+        for row in data_:
+            sqlite_insert_str = """INSERT INTO IdentifierTable
+                                    (ID, AGE)
+                                    VALUES
+                                    ({}, {});"""
+
+            sqlite_insert_query = sqlite_insert_str.format(row[0], row[1])
+            count = cursor.execute(sqlite_insert_query)
+            self.conn.commit()
+        cursor.close()
+
+
+    # Function To Return Number Of Rows In Database
+    def rowsinDB(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM IdentifierTable;")
+        print(len(cursor.fetchall()))
+        cursor.close()
+
+
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Main Entry Point Into Python Code
 if __name__ == "__main__":
     SQL_Connection = SQL_Database()
-
-
-
-
-"""
-
-conn.execute('''CREATE TABLE COMPANY
-         (ID INT PRIMARY KEY     NOT NULL,
-         NAME           TEXT    NOT NULL,
-         AGE            INT     NOT NULL,
-         ADDRESS        CHAR(50),
-         SALARY         REAL);''')
-
-"""
+    SQL_Connection.addtoDB([[123, 21], [123, 21], [123, 21], [123, 21], [123, 21], [123, 21], [123, 21], [123, 21]])
+    SQL_Connection.rowsinDB()
