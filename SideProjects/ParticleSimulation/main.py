e098818e6b33e93367916403da4d6386b2e94516
renacin <renacin.matadeen@ryerson.ca>
2020-12-16 19:56:16 -0500

    Pyglet Is Pretty Intimidating

--- SideProjects/ParticleSimulation/main.py
+++ SideProjects/ParticleSimulation/main.py
@@ -5,26 +5,40 @@
 # ----------------------------------------------------------------------------------------------------------------------
 import time
 import pyglet
+from pyglet.gl import *
 import math
 # ----------------------------------------------------------------------------------------------------------------------
 """
 Notes:
     + Following:
-        - https://www.youtube.com/watch?v=6JPsrwfX8pw
+        - https://www.youtube.com/watch?v=Wyv5TnkFuxE
 
 """
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Basic Setup For A Simple Particle Simulator
-def main():
-    window = pyglet.window.Window(800, 800, "Pyglet Test Window") # Change Window Size
-    pyglet.app.run()
+class Triangle:
+    def __init__(self):
+        self.vertices = pyglet.graphics.vertex_list(3, ("v3f", [-0.5, -0.5, 0.0,   0.5, -0.5, 0.0,   0.0, 0.5, 0.0]),
+                                                       ("c3B", [100, 200, 220,  200, 110, 100,  100, 250, 100])
+                                                       )
 
 
 
 
+# Use Pyglet As ENV, Create Window Class
+class MainWindow(pyglet.window.Window):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_minimum_size(200, 200)
+        self.triangle = Triangle()
+
+    def on_draw(self):
+        self.triangle.vertices.draw(GL_TRIANGLES)
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Main Entry Point Into Python Code
 if __name__ == "__main__":
-    main()
+    window = MainWindow(800, 800, "Main Window", resizable=True)
+    window.on_draw()
+    pyglet.app.run()
