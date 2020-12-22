4d32562062ebcb4ae9f8bed3e45af544bcde8159
renacin <renacin.matadeen@ryerson.ca>
2020-12-21 21:20:07 -0500

    Created A Shittier Version Of Pong; Hitboxes Are Pretty Cool Though

--- SideProjects/ParticleSimulation/hitboxdetection.py
+++ SideProjects/ParticleSimulation/hitboxdetection.py
@@ -10,8 +10,26 @@ from pygame.locals import *
 # ----------------------------------------------------------------------------------------------------------------------
 
 
+# Move Player Icon
+def move_player(screen, screen_dimensions, player_rect, colour):
+    mx, my = pygame.mouse.get_pos()
+
+    # Player Collision With Screen Borders
+    if (player_rect.right >= screen_dimensions[0]):
+        player_rect.x = screen_dimensions[0] - 200
+
+    elif (player_rect.left <= 0):
+        player_rect.x = 0 + 200
+
+    else:
+        player_rect.x = mx
+
+    pygame.draw.rect(screen, colour, player_rect)
+
+
+
 # Moving Rectangles
-def move_rectangles(screen, rect, x_speed, y_speed, screen_dimensions, colour, list_of_hitboxes):
+def move_rectangles(screen, rect, x_speed, y_speed, screen_dimensions, colour, list_of_hitboxes, num_col):
 
     rect.x += x_speed
     rect.y += y_speed
@@ -25,7 +43,7 @@ def move_rectangles(screen, rect, x_speed, y_speed, screen_dimensions, colour, l
 
 
     # Check Collisions With Other Shapes
-    col_tol = 15
+    col_tol = 5
     for shape_ in list_of_hitboxes:
         if rect != shape_:
             if rect.colliderect(shape_):
@@ -33,23 +51,27 @@ def move_rectangles(screen, rect, x_speed, y_speed, screen_dimensions, colour, l
                 # Top Collision
                 if abs(rect.top - shape_.bottom) < col_tol:
                     y_speed *= -1
+                    num_col += 1
 
                 # Bottom Collision
                 if abs(rect.bottom - shape_.top) < col_tol:
                     y_speed *= -1
+                    num_col += 1
 
                 # Left Collision
                 if abs(rect.left - shape_.right) < col_tol:
                     x_speed *= -1
+                    num_col += 1
 
                 # Right Collision
                 if abs(rect.right - shape_.left) < col_tol:
                     x_speed *= -1
+                    num_col += 1
 
 
     pygame.draw.rect(screen, colour, rect)
 
-    return x_speed, y_speed
+    return x_speed, y_speed, num_col
 
 
 
@@ -67,18 +89,23 @@ def main():
     screen = pygame.display.set_mode(screen_dimensions)
 
     # Create Moving Rectangle
-    moving_rect = pygame.Rect(350, 350, 100, 100)
+    moving_rect = pygame.Rect(350, 350, 10, 10)
     m_colour = (255, 255, 255)
     x_speed, y_speed = 5, 4
 
     # Create Stationary Rectangle
-    stationary_rect = Rect(300, 600, 200, 200)
+    stationary_rect = Rect(300, 600, 10, 10)
     s_colour = (255, 0, 0)
     oth_x_speed, oth_y_speed = 5, 4
 
-    list_of_hitboxes = [moving_rect, stationary_rect]
+    # Create Player Sprite
+    player_rect = Rect(100, 900, 100, 10)
+    p_colour = (0, 255, 0)
+
+    list_of_hitboxes = [moving_rect, stationary_rect, player_rect]
 
     # Main Pygame Loop | Need To Check For Events
+    num_col = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -87,9 +114,11 @@ def main():
 
         screen.fill(screen_colour)
 
-        x_speed, y_speed = move_rectangles(screen, moving_rect, x_speed, y_speed, screen_dimensions, m_colour, list_of_hitboxes)
-        oth_x_speed, oth_y_speed = move_rectangles(screen, stationary_rect, oth_x_speed, oth_y_speed, screen_dimensions, s_colour, list_of_hitboxes)
+        move_player(screen, screen_dimensions, player_rect, p_colour)
+        x_speed, y_speed, num_col = move_rectangles(screen, moving_rect, x_speed, y_speed, screen_dimensions, m_colour, list_of_hitboxes, num_col)
+        oth_x_speed, oth_y_speed, num_col = move_rectangles(screen, stationary_rect, oth_x_speed, oth_y_speed, screen_dimensions, s_colour, list_of_hitboxes, num_col)
 
+        print("Number Of Collisions: {}".format(num_col))
         pygame.display.flip()
         clock.tick(FPS)
 
