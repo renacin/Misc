09dfc5b5b53eb37e048cf7740b0b5033947dfef6
Renacin <renacinm@gmail.com>
2021-09-19 18:21:41 -0400

    Finish Data Comparison & Send Off Data

--- SideProjects/CityOfToronto_Tools/HeritageRegistryDataQA/main.py
+++ SideProjects/CityOfToronto_Tools/HeritageRegistryDataQA/main.py
@@ -7,14 +7,25 @@ from funcs.process import *
 # ----------------------------------------------------------------------------------------------------------------------
 
 def main():
+    """ Main logic of this python script. Import data, clean data, and then create comparison datasets from original
+    data. Run if name equals main. """
 
     # Sources Of Data
     ibms_csv = r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\CityOfToronto_Tools\HeritageRegistryDataQA\Data\DataFromIBMSTeam.csv"
     heritage_csv = r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\CityOfToronto_Tools\HeritageRegistryDataQA\Data\DataFromHeritageTeam.csv"
 
+
     # Ingest & Filter Only Address & PRSN Column
     from_ibms_df = Dataset.filter_data(ibms_csv)
     from_heritage_df = Dataset.filter_data(heritage_csv)
+
+
+    # Remove Duplicates From PRSN (Keep Only Primary Addresses)
+    no_dup_ibms_df = Dataset.remove_dup(from_ibms_df, "IBMS Data")
+    no_dup_heritage_df = Dataset.remove_dup(from_heritage_df, "Heritage Data")
+
+
+    # Compare Both Datasets, Which Are Common, Which Can Only Be Found In Both
 
 
 # ----------------------------------------------------------------------------------------------------------------------
