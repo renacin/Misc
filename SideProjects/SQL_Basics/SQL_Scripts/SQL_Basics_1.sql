865e4d5164118d80edf7d3cd3c2c37bc6aa599ae
Renacin <renacinm@gmail.com>
2021-09-22 19:16:13 -0400

    Getting back into sql basics

---
+++ SideProjects/SQL_Basics/SQL_Scripts/SQL_Basics_1.sql
@@ -0,0 +1,18 @@
+/*
+Name:                                            Renacin Matadeen
+Date:                                               09/22/2021
+Title                                      Review Of Core SQL Concepts
+
+----------------------------------------------------------------------------------------------------------------------
+
+History Of SQL:
+    + 
+
+Notes:
+    +
+
+Useful Websites:
+    + 
+
+
+*/
