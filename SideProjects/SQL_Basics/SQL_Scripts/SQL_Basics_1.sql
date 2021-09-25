2c6efbe420d58bdd8f9f101c6c6d0288db1e821e
Renacin <renacinm@gmail.com>
2021-09-25 12:51:34 -0400

    Database development has a deep and very intersting history

--- SideProjects/SQL_Basics/SQL_Scripts/SQL_Basics_1.sql
+++ SideProjects/SQL_Basics/SQL_Scripts/SQL_Basics_1.sql
@@ -1,18 +1,30 @@
 /*
 Name:                                            Renacin Matadeen
-Date:                                               09/22/2021
+Date:                                               09/25/2021
 Title                                      Review Of Core SQL Concepts
 
 ----------------------------------------------------------------------------------------------------------------------
 
-History Of SQL:
-    + 
-
 Notes:
-    +
+    The Importance Of Databases
+        Information is everywhere. All of our interactions produce data; and there is an increasing need to store the
+        data that we create.
+
+        Heirarchical Databases
+            + Computerized databases systems were first introduced in the early 1960s. The most successful of the first database
+            systems was the SABRE storage methodology introduced by IBM. 
+            + Note that these early databases were hierarchical in
+            nature; this meant that all entries were stored with some kind of a connection to a parent node. These types of databases
+            are very simple in nature. They resemble the structure of tree data stuctures, as well as directories within a computer
+            system.
+            + Data could only have parent/child relationships. A child could only have one parent, but parent multiple childs
+            + In many cases a many-to-one relationship would be highly benefitial; ie two parents having relationships to one child
+
+        Relational Databases
+        - The first relational database model was proposed back in the 1970s as an alternative to hierarchical models. 
+
 
 Useful Websites:
     + 
-
 
 */
