02210208c0e01272e3cdbd62bef94dd84287f771
Renacin <renacinm@gmail.com>
2022-01-15 14:34:28 -0500

    New Project

---
+++ SideProjects/CityOfToronto_Tools/GraphicsReportAutomation_QCQA/main.py
@@ -0,0 +1,38 @@
+# Name:                                            Renacin Matadeen
+# Date:                                               01/07/2022
+# Title                        City Of Toronto: Graphics & Visualization File Crawler
+#
+# ----------------------------------------------------------------------------------------------------------------------
+# import datetime
+# import csv
+# import os
+# import pandas as pd
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class ClassStorage:
+    """ This class stores all functions of this file crawler"""
+
+    def __init__(self):
+        """ On instantiation... """
+        pass
+
+    # ------------------------------------------------------------------------------------------------------------------
+    #   PRIVATE METHODS
+    def __priv_func(self, var):
+        """ Details """
+
+
+    # ------------------------------------------------------------------------------------------------------------------
+    #   PUBLIC METHODS
+    def pub_func(self, var):
+        """ Details """
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Main Entry Point
+if __name__ == "__main__":
+
+    # Main Logic Of Code
+    print("Hello")
