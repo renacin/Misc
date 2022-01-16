548b759bc4da470baf41cec4f8231e55316a1551
Renacin <renacinm@gmail.com>
2022-01-16 13:57:43 -0500

    This shouldn't be too hard

--- SideProjects/CityOfToronto_Tools/GraphicsReportAutomation_QCQA/Scripts/main.py
+++ SideProjects/CityOfToronto_Tools/GraphicsReportAutomation_QCQA/Scripts/main.py
@@ -3,19 +3,13 @@
 # Title                        City Of Toronto: Graphics & Visualization File Crawler
 #
 # ----------------------------------------------------------------------------------------------------------------------
-# import datetime
-# import csv
-# import os
-# import pandas as pd
+import datetime
+import pandas as pd
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-class ClassStorage:
-    """ This class stores all functions of this file crawler"""
-
-    def __init__(self):
-        """ On instantiation... """
-        pass
+class QC_Checker:
+    """ This class stores all functions of the Graphics & Visualization """
 
     # ------------------------------------------------------------------------------------------------------------------
     #   PRIVATE METHODS
@@ -25,8 +19,17 @@ class ClassStorage:
 
     # ------------------------------------------------------------------------------------------------------------------
     #   PUBLIC METHODS
-    def pub_func(self, var):
-        """ Details """
+    def gather_data(self, data_path: str) -> "None":
+        """
+        Notes:
+            Given the path of the folder that contains all XLSX documents that contain addresses passed through
+            the graphics request FME automation script, combine all XLSX documents into one Pandas Dataframe.
+            Dataframe cached as class static variable.
+            applications.
+
+        In:
+
+        """
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -35,4 +38,5 @@ class ClassStorage:
 if __name__ == "__main__":
 
     # Main Logic Of Code
-    print("Hello")
+    qc_init = QC_Checker()
+    qc_init.gather_data()
