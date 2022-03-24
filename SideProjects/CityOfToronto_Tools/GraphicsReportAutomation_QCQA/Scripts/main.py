9adb3d301c82011ca8ff3b8b4f7a525c9033ce69
Renacin <renacinm@gmail.com>
2022-03-24 13:11:07 -0400

    Fixed date issue

--- SideProjects/CityOfToronto_Tools/GraphicsReportAutomation_QCQA/Scripts/main.py
+++ SideProjects/CityOfToronto_Tools/GraphicsReportAutomation_QCQA/Scripts/main.py
@@ -8,83 +8,119 @@ from itertools import cycle
 from openpyxl.workbook import Workbook
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
+
 # ----------------------------------------------------------------------------------------------------------------------
+#   PRIVATE FUNCTION
+def __standardize_date(in_dates: list) -> list:
+    """
+    Notes:
+        Identified an issue with date format going from Excel to CSV, and back to Excel.
+        This private function will standardize the dates to a single format.
 
-class QC_Checker:
-    """ This class stores all functions of the Graphics & Visualization """
+        Date Formats Found:
+            1) [YYYY-MM-DD]
+            2) [DD/MM/YYYY]
+            3) [MM-DD-YYYY]
 
-    rd = None
-    current_date = str(datetime.date.today().strftime("%Y-%m-%d")).split(" ")[0]
+        Standardized Date Format:
+            1) [YYYY-MM-DD]
 
-    # ------------------------------------------------------------------------------------------------------------------
-    #   PRIVATE METHODS
-    def __filechecker(self, file_, data_path) -> bool:
-        """
-        Notes:
-            Checks are conducted to ensure the files provided fit the appropriate format:
-                + XLSX
-                + Contain Headers & Footers
-                + Same Columns & Datatypes between files
+    Input:
+        in_dates:        Pandas Column -->    List of dates that needs to be standardized
 
-        Input:
-            file_path: string --> Path To File
+    Output:
+        out_dates:       list          -->    Cleaned list of dates
+    """
 
-        Output:
-            bool: True/False --> Is the file appropriate, given requirements?
-        """
+    cleaned_dates = []
+    for date_ in in_dates.values.tolist():
 
-        # Make Sure Path Points To File, Filename Is Appropriate, Is CSV Doc, & Has Appropriate Headers/Footers
-        if os.path.isdir(file_) is False:
-            if " ".join(file_.split(".")[0].split()[3:]) == "All Applications Received - Extract":
-                if (file_.split(".")[-1] in ["csv", "CSV"]):
-                    with open((data_path + "\\" + file_), mode ='r') as file:
-                        csv_file = csv.reader(file)
-                        for line in csv_file:
-                            if line[0] == "IBMS Reports":
-                                return True
+        # [DD/MM/YYYY]
+        if "/" in date_:
+            sep_date = date_.split("/")
+            year_ = sep_date[-1]
+            month_ = sep_date[1]
+            day_ = sep_date[0]
 
-        print(f"File Error Detected: {file_}")
-        return False
+        else:
+            sep_date = date_.split("-")
 
+            # [MM-DD-YYYY]
+            if date_[-4:].isnumeric():
+                year_ = sep_date[-1]
+                month_ = sep_date[0]
+                day_ = sep_date[1]
 
-    # ------------------------------------------------------------------------------------------------------------------
-    #   PUBLIC METHODS
-    def gather_data(self, data_path: str, export_folder: str) -> "None":
-        """
-        Notes:
-            Given the path of the folder that contains all CSV documents that contain addresses passed through
-            the graphics request FME automation script, combine all CSV documents into one Pandas Dataframe.
-            Dataframe cached as class static variable.
+            # [YYYY-MM-DD]
+            else:
+                year_ = sep_date[0]
+                month_ = sep_date[1]
+                day_ = sep_date[-1]
 
-        Input:
-            data_path:       string -->    Path To Folder Containing Weekly Update Xlsx Files
-            export_folder:   string -->    Path To Folde Containing 4 District Output Files
+        # [YYYY-MM-DD]
+        cleaned_dates.append(f"{year_}-{month_}-{day_}")
 
-        Output:
-            None --> Resulting dataframe is stored within Class as static variable. Use method to access.
-
-        Update:
-            [2022-03-24 | Renacin Matadeen] - Added Functionality To Create 1 masterfile from both weekly
-                                              application list, as well as all 4 district outputs
-
-        """
-
-        # Given Path, Loop Through Folder & Combine CSV Files | Ensure Checks For Appropriate Files!
-        frames = []
-        directory_ = os.listdir(data_path)
-        for file_ in directory_:
-            if self.__filechecker(file_, data_path):
-                add_df = pd.read_csv(data_path + "\\" + file_, skiprows= 6)
-                for col in add_df.columns:
-                    add_df[col] = add_df[col].astype(str)
-                frames.append(add_df)
-
-        merged_df = pd.concat(frames)
-        merged_df = merged_df.drop_duplicates(subset = ["File Number"], keep = "first")
-        QC_Checker.rd = merged_df
+    return cleaned_dates
 
 
-        # Look For All 4 District Files & Concatinate Back Into 1 File
+
+# ------------------------------------------------------------------------------------------------------------------
+#   PUBLIC FUNCTION
+def gather_data(data_path: str, export_folder: str) -> "None":
+    """
+    Notes:
+        Given the path of the folder that contains all CSV documents that contain addresses passed through
+        the graphics request FME automation script, combine all CSV documents into one Pandas Dataframe.
+        Dataframe cached as class static variable.
+
+    Input:
+        data_path:       string -->    Path To Folder Containing Weekly Update Xlsx Files
+        export_folder:   string -->    Path To Folde Containing 4 District Output Files
+
+    Output:
+        None --> Resulting dataframe is stored within Class as static variable. Use method to access.
+
+    Update:
+        [2022-03-24 | Renacin Matadeen ] - Added Functionality To Create 1 masterfile from both weekly
+                                           application list, as well as all 4 district outputs
+
+    """
+
+    # Given Path, Loop Through Folder & Combine CSV Files | Ensure Checks For Appropriate Files!
+    frames = []
+    directory_ = os.listdir(data_path)
+    for file_ in directory_:
+        if "~$" not in str(file_):
+            weekly_df = pd.read_csv(data_path + "\\" + file_, skiprows= 6)
+            for col in weekly_df.columns:
+                weekly_df[col] = weekly_df[col].astype(str)
+            frames.append(weekly_df)
+
+    w_merged_df = pd.concat(frames)
+    weekly_merged_df = w_merged_df.drop_duplicates(subset = ["File Number"], keep = "first")
+    weekly_merged_df["InDate"] = __standardize_date(weekly_merged_df["InDate"])
+
+
+    # Look For All 4 District Files & Concatinate Back Into 1 File
+    m_frames = []
+    directory_ = os.listdir(export_folder)
+    for d_file in directory_:
+
+        # Skip If File Is Excel temp File
+        if "~$" not in str(d_file):
+            district_df = pd.read_excel(export_folder + "\\" + d_file)
+
+            # Format Columns To String & Create New Dataset
+            for col in district_df.columns:
+                district_df[col] = district_df[col].astype(str)
+            m_frames.append(district_df)
+
+    districts_merged_df = pd.concat(m_frames)
+    districts_merged_df = districts_merged_df.replace("nan", "")
+    districts_merged_df = districts_merged_df.drop_duplicates(subset = ["File Number"], keep = "first")
+    districts_merged_df["InDate"] = __standardize_date(districts_merged_df["InDate"])
+    districts_merged_df.to_csv(r"C:\Users\renac\Desktop\TEST.csv", index = False)
+
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -97,13 +133,16 @@ if __name__ == "__main__":
     export_folder = r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\CityOfToronto_Tools\GraphicsReportAutomation_QCQA\Data\MasterOutputs"
 
     # Main Logic Of QC Checker
-    qc_init = QC_Checker()
-    qc_init.gather_data(data_path)
+    gather_data(data_path, export_folder)
 
 
 
     # # qc_init.check_data(export_folder)
     # qc_init.export_data(export_folder)
+
+    # current_date = str(datetime.date.today().strftime("%Y-%m-%d")).split(" ")[0]
+
+
 
 
 
