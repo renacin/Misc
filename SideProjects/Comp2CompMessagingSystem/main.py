af7251231eafd99c8ca7bbd80d948ed62d5db012
Renacin <renacinm@gmail.com>
2023-01-07 11:28:41 -0500

    Getting back into the swing of things?

---
+++ SideProjects/Comp2CompMessagingSystem/main.py
@@ -0,0 +1,23 @@
+# Name:                                            Renacin Matadeen
+# Date:                                               01/07/2023
+# Title                                 Computer To Computer Messaging System
+#
+# ----------------------------------------------------------------------------------------------------------------------
+import os, datetime, csv
+import pandas as pd
+# ----------------------------------------------------------------------------------------------------------------------
+#   PRIVATE FUNCTION
+def main():
+    """ This is a template """
+    print("Hello World")
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# Main Entry Point
+if __name__ == "__main__":
+
+    # Needed Variables
+    main()
