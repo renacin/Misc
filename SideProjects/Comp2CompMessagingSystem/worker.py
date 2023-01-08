9c5a6650530b78792d12998787aea5e4cf37113f
Renacin <renacinm@gmail.com>
2023-01-08 16:17:11 -0500

    Getting there, having an issue with the threading

--- SideProjects/Comp2CompMessagingSystem/worker.py
+++ SideProjects/Comp2CompMessagingSystem/worker.py
@@ -22,34 +22,49 @@ class Worker:
         Will There Be A Case Where We Have Two Workers With main_worker_status == True, Hopefully Not
         """
 
+        # Worker Attributes
         self.id_name = random.randrange(0, 1000, 1)
-        self.log_path = r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\Comp2CompMessagingSystem\LOGS"
         self.main_worker_status = False
         self.SENTINEL = "&-99-&"
+
+        # File Variables
+        self.log_path = r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\Comp2CompMessagingSystem\LOGS"
+        lst_attest = r"C:\Users\renac\Documents\Programming\Python\Misc\SideProjects\Comp2CompMessagingSystem\MISC\ListOfAutoTests.csv"
+        self.autotests = pd.read_csv(lst_attest)
+
+        now = datetime.datetime.now()
+        self.workerlog = f"WORKER_LOG_{now.year}_{now.month}_{now.day}.txt"
+        self.logginglog = f"COMP_LOG_{now.year}_{now.month}_{now.day}.txt"
 
 
 
     # Worker Should Be Able To Check In Constantly Given It's Own Thread
     def check_in(self):
-        """ This Class Method Will Be Used To Update The Workers main_worker_status variable
+        """ This Class Method Will Be Used To Update The Workers main_worker_status variable & Worker Log File
 
         Cases:
-            Worker Has Just Been Initialized,                  main_worker_status = False
-            Worker Checks Log File And Sees Someone Working,   main_worker_status = False
-            Worker Checks Log File And Sees No One Working,    main_worker_status = True
+            IF main_worker_status == TRUE , THEN WRITE TO WORKER LOG FILE
+            IF main_worker_status == FALSE,
+                AND WORKER LOG FILE EXISTS,
+                AND LAST UPDATE GREATER THAN 10 SEC,
+                THEN WRITE TO WORKER LOG FILE,
+                AND main_worker_status == TRUE
+            IF main_worker_status == FALSE,
+                AND WORKER LOG FILE NOT EXISTS,
+                THEN WRITE TO WORKER LOG FILE,
         """
         # Check Time
         now = datetime.datetime.now()
 
         # If Previous Check-In Attempt Changed main_worker_status Worker Is Main Worker
         if self.main_worker_status == True:
-            with open(f"{self.log_path}\\WORKER_LOG_{now.year}_{now.month}_{now.day}.txt", "a") as f:
+            with open(f"{self.log_path}\\{self.workerlog}", "a") as f:
                 f.write(f"[WORKSTAT], {self.id_name}, {datetime.datetime.now()}, WORKING, {self.SENTINEL} \n")
             return
 
-        # Check To See If Worker Check-In LOG Exists | Note Logs Are Day Specific
-        if f"WORKER_LOG_{now.year}_{now.month}_{now.day}.txt" in os.listdir(self.log_path):
-            with open(f"{self.log_path}\\WORKER_LOG_{now.year}_{now.month}_{now.day}.txt", "r") as f:
+        # If Still False Check To See If Worker Check-In LOG Exists
+        if f"{self.workerlog}" in os.listdir(self.log_path):
+            with open(f"{self.log_path}\\{self.workerlog}", "r") as f:
                 lines = f.readlines()
 
                 # If Log Exists Check If Another Worker Has Been Working In The Last 10 Seconds
@@ -58,17 +73,16 @@ class Worker:
 
                 if wrk_dif.seconds > 10:
                     self.main_worker_status = True
-                    with open(f"{self.log_path}\\WORKER_LOG_{now.year}_{now.month}_{now.day}.txt", "a") as f:
+                    with open(f"{self.log_path}\\{self.workerlog}", "a") as f:
                         f.write(f"[WORKSTAT], {self.id_name}, {datetime.datetime.now()}, TAKING CONTROL, {self.SENTINEL} \n")
+                    return
 
         # If File Doesn't Exist For The Day Then Worker Is Worker Is Main | Write To Text File
         else:
             self.main_worker_status = True
-            with open(f"{self.log_path}\\WORKER_LOG_{now.year}_{now.month}_{now.day}.txt", "a") as f:
+            with open(f"{self.log_path}\\{self.workerlog}", "a") as f:
                 f.write(f"[WORKSTAT], {self.id_name}, {datetime.datetime.now()}, FIRST WORKER, {self.SENTINEL} \n")
-
-        return
-
+            return
 
 
     # Worker Should Be Able To Check In Constantly Given It's Own Thread
@@ -78,14 +92,43 @@ class Worker:
         Cases:
             If main_worker_status = False, Don't Do Work    - Wait For Check-In To Return Different Value
             If main_worker_status = True , Start Doing Work - Either Start At Begining, Or Pick Off Where Other Worker Left Off
-         """
+        """
 
-        # Based Of main_worker_status Start Working Or Exit To Thread Loop
-        if self.main_worker_status == True:
-            print(f"Worker {self.id_name}: Working")
-        else:
-            print(f"Worker {self.id_name}: Not Working")
+        try:
+            # Based Of main_worker_status Start Working Or Exit To Thread Loop
+            if self.main_worker_status == True:
 
+                # LOG File Exists & What The Last Test Was | Note Logs Are Day Specific
+                now = datetime.datetime.now()
+                if f"{self.logginglog}" in os.listdir(self.log_path):
+                    with open(f"{self.log_path}\\{self.logginglog}", "r") as f:
+
+                        # File Has Some Lines
+                        if len(f.readlines()) != 0:
+                            lines = f.readlines()
+                            max_idx = int(lines[-1].split(",")[1])
+
+                        # File Has No Lines
+                        elif len(f.readlines()) == 0:
+                            max_idx = 0
+
+                # LOG File Does Not Exist
+                else:
+                    max_idx = 0
+
+                # Based On Previous Logs Written (In The Same Day) Focus Only On The AutoTests That Haven't Been Run
+                self.autotests = self.autotests.iloc[max_idx:]
+
+                # Do Work And Make Log When File Is Done Running
+                f = open(f"{self.log_path}\\{self.logginglog}", "a")
+                for index, row in self.autotests.iterrows():
+                    time.sleep(row["Time"])
+                    f.write(f"[COMPSTAT], {row['ID']}, {row['TestName']}, {datetime.datetime.now()}, {self.SENTINEL} \n")
+                    print(row["TestName"])
+                f.close()
+
+        except Exception:
+            raise Exception
 
 
     # Worker Should Be Able To Check In Constantly Given It's Own Thread
@@ -95,11 +138,9 @@ class Worker:
         c_var = True
         while c_var:
             try:
-                # Create Thread To Run Check In Process
+                # Create For Check-In & Work Process
                 t1 = threading.Thread(target=self.check_in)
                 t1.start()
-
-                # Create Thread To Run Start Working
                 t2 = threading.Thread(target=self.work)
                 t2.start()
 
