adbb2b9058525042c13b1fc933332294cc8bcf2b
Renacin <renacinm@gmail.com>
2023-01-08 11:06:08 -0500

    Making small progress

--- SideProjects/Comp2CompMessagingSystem/main.py
+++ SideProjects/Comp2CompMessagingSystem/main.py
@@ -3,13 +3,25 @@
 # Title                                 Computer To Computer Messaging System
 #
 # ----------------------------------------------------------------------------------------------------------------------
-import os, datetime, csv
+import os, datetime, csv, time
 import pandas as pd
 # ----------------------------------------------------------------------------------------------------------------------
-#   PRIVATE FUNCTION
+
+def run_auto_test(list_of_tests):
+    for test in list_of_tests:
+        time.sleep(test[1])
+        print(f"Completed {test[0]}")
+
+# ----------------------------------------------------------------------------------------------------------------------
 def main():
-    """ This is a template """
-    print("Hello World")
+    """ This Is The Main Logic Of This Computer To Computer Messaging System"""
+
+    # Step 1) Identify List Of Autotests To Run
+    lst_attest = [("Amazon", 5.5), ("SmartCash", 4.5), ("Nordstrom", 2.5), ("AirMiles", 4.5), ("CashBack", 2.5), ("Business", 2.5)]
+
+    # Step 2) Run AutoTests & Print Comletion Status
+    run_auto_test(lst_attest)
+
 
 
 
